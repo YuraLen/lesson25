5da08fded0ab3f129724f9c646f29d120f262c00
YuraLen <92933581+YuraLen@users.noreply.github.com>
2021-11-04 13:13:55 +0200

    7 задание | Готово

--- js/funcions.js
+++ js/funcions.js
@@ -5,4 +5,37 @@ function dowbleElem(){
   console.log(result);
 }
 
-dowbleElem()
+function notAnArrey(arr){
+  if(!Array.isArray(arr)){
+    return 'error';
+  }
+}
+
+function nonNumericElementsInArray(arr) {
+  function isntNumber(value){
+    return typeof value === 'string';
+  }
+  if(arr.every(isntNumber)){
+    return 'error';
+  }
+
+  // arr.forEach((item) => {
+  //   if(typeof item === 'number'){
+  //     return 'error';
+  //   }
+  // });
+
+  // if(arr.filter(item => typeof item === 'number')){
+  //   return 'error';
+  // }
+  // if(typeof (arr) !== 'number'){
+  //   return 'error';
+  // } 
+}
+
+function moreThanNumericElem(arr){
+  if(arr.filter(item => typeof item !== 'number' && typeof item !== 'string')){
+    return 'error';
+  }
+}
+
--- js/test.js
+++ js/test.js
@@ -1,5 +1,13 @@
 describe("Тесты с массивами", function() { 
 	it("Передача не массива", () => { 
-		assert.equal(pow(2, 3), 8); 
+		assert.equal(notAnArrey('1', '2'), 'error'); 
 	}); 
+
+  it("В массиве нечисловые элементы", () => {
+    assert.equal(nonNumericElementsInArray(['hello']), 'error');
+  })
+
+  it("В массиве не только числовые элементы.", () => {
+    assert.equal(moreThanNumericElem([1, 'hello']), 'error')
+  })
 });
